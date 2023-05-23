d42e08b4a9f210c23442c8925692ae6cc037d841
Gururaj KL <gururajragavendra7@gmail.com>
2023-05-23 09:42:29 +0530

    Add project files.

---
+++ WPFThemes/AppTheme.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows;
+
+namespace WPFThemes
+{
+    /// <summary>
+    /// Sets the theme of the application.
+    /// </summary>
+    public class AppTheme
+    {
+        /// <summary>
+        /// Changes the theme dynamically at run time.
+        /// </summary>
+        /// <param name="themeUri">Relative position of the resource dictionary.</param>
+        public static void SwitchTheme(Uri themeUri)
+        {
+            ResourceDictionary resourceDictionary = new ResourceDictionary()
+            {
+                Source = themeUri,
+            };
+
+            App.Current.Resources.Clear();
+            App.Current.Resources.MergedDictionaries.Add(resourceDictionary);
+        }
+    }
+}
---
+++ WPFThemes/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows;
+
+namespace WPFThemes
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LightThemeClick(object sender, RoutedEventArgs e)
+        {
+            AppTheme.SwitchTheme(new Uri("Themes/Light.xaml", UriKind.Relative));
+        }
+
+        private void DarkThemeClick(object sender, RoutedEventArgs e)
+        {
+            AppTheme.SwitchTheme(new Uri("Themes/Dark.xaml", UriKind.Relative));
+        }
+    }
+}
